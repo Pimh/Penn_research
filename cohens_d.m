c2ff3269a770cc618be9b91f972b18dd9148687c
Pimh <pimh@seas.upenn.edu>
2018-01-31 17:28:52 -0500

    added cohens_d.m

---
+++ cohens_d.m
@@ -0,0 +1,13 @@
+function d = cohens_d(X,Y)
+
+X = X(~isnan(X));
+Y = Y(~isnan(Y));
+Mx = nanmean(X);
+My = nanmean(Y);
+nx = length(X);
+ny = length(Y);
+
+sx = 1/(nx-1) * sum((X - Mx).^2);
+sy = 1/(ny-1) * sum((Y - My).^2);
+SD_pool = sqrt(((nx-1)*sx + (ny-1)*sy)/(nx+ny-2));
+d = (Mx - My)/SD_pool;
